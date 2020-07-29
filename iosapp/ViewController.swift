dcf3bc4d85467210854452668a25ee561d0fbbe1
Ananth <16939018+AnanthaKrish@users.noreply.github.com>
2020-07-29 11:37:25 +0530

    new code changes

--- iosapp/ViewController.swift
+++ iosapp/ViewController.swift
@@ -2,34 +2,26 @@
 //  ViewController.swift
 //  iosapp
 //
+//  Created by Anantha Krishnan K G on 29/07/20.
+//  Copyright © 2020 Ananth. All rights reserved.
+//
 
 import UIKit
 import BMSCore
-
-
-
-
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
-        NotificationCenter.default.addObserver(self, selector: #selector(didBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
-
+        // Do any additional setup after loading the view.
         
-
-        
-
-        
+         NotificationCenter.default.addObserver(self, selector: #selector(didBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
-
+    
     @objc func didBecomeActive(_ notification: Notification) {
-        
-        
+           
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -37,3 +29,4 @@ class ViewController: UIViewController {
 
 
 }
+
